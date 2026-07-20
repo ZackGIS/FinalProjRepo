49fbc5f2ecc2d89909246cd60053e67bab439296
ZackGIS <znelso134@gmail.com>
2026-07-20 15:59:37 -0500

    Update GeoJSONLoader.cs
    1st major revision

--- GeoJSONLoader.cs
+++ GeoJSONLoader.cs
@@ -5,14 +5,21 @@ using Esri.ArcGISMapsSDK.Components;
 using Newtonsoft.Json.Linq;
 using System.Collections;
 using Esri.HPFramework;
+using System.Collections.Generic;
+using UnityEngine.Timeline;
+using UnityEngine.LowLevelPhysics;
 
 public class GeoJSONLoader : MonoBehaviour
 {
-    public string geoJSONFilePath = @"C:\Users\theim\PycharmProjects\pythonProject4\locations.geojson"; //file path for geojson
+    public string[] geoJSONFilePaths = { @"C:\Users\theim\PycharmProjects\pythonProject4\locations.geojson", 
+        @"C:\Users\theim\PycharmProjects\pythonProject4\s2underground_posts.geojson", 
+        @"C:\Users\theim\PycharmProjects\pythonProject4\article_events.geojson" }; //file paths for geojsons
     public GameObject pointPrefab;   //declaring GameObject variable. Each feature in the geojson file will be created
 
-    private float defaultAltitude = 1000.0f; // Default is set to 1000ft. Want to ensure the features are visible for testing.
+    //private float defaultAltitude = 5.0f; // Default is set to 1000ft. Want to ensure the features are visible for testing.
     public ArcGISMapComponent map;          //ArcGISMpaComponent object
+
+    public List<FeatureInfo> loadedFeatures = new List<FeatureInfo>();
 
     void Start()
     {
@@ -24,7 +31,6 @@ public class GeoJSONLoader : MonoBehaviour
 
             if (map != null)
             {
-                Debug.Log("ArcGISMapComponent successfully assigned.");
                 StartCoroutine(WaitForMapInitialization());
             }
             else
@@ -44,59 +50,56 @@ public class GeoJSONLoader : MonoBehaviour
     {
         while (!map.gameObject.activeInHierarchy)
         {
-            Debug.Log("Waiting for ArcGISMapComponent to activate...");
-            yield return null; // Wait for the next frame
+            yield return null; //Wait for the next frame
         }
 
-        Debug.Log("ArcGISMapComponent is active.");
-        LoadGeoJSON();
+        foreach(string filepath in geoJSONFilePaths) 
+        {
+            LoadGeoJSON(filepath);
+        }
+       
     }
 
     // Function below to load the geojson and parse its geospatial data
-    private void LoadGeoJSON()
+    private void LoadGeoJSON(string filepath)
     {
-        string geoJSONText = File.ReadAllText(geoJSONFilePath); //create an object to read text from the input file path defined in variables section.
+        string geoJSONText = File.ReadAllText(filepath); //create an object to read text from the input file path defined in variables section.
         JObject geoJSONData = JObject.Parse(geoJSONText); //JObject and the Parse() fucntion are from the Newtonsoft Json library. in this case I'm parsing the 
-                                                           // text from the file path and storing that in a JObject called geoJSONData
-
-        foreach (var feature in geoJSONData["features"])  //loop through all of the features in geoJSONData
+                                                          // text from the file path and storing that in a JObject called geoJSONDa
+      
+        foreach (JObject featureObject in geoJSONData["features"])
         {
-            var coordinates = feature["geometry"]["coordinates"]; //coordinates are nested within the "geometry" attribute
-            double lat = (double)coordinates[0];                    //cast lat and lon as a double
-            double lon = (double)coordinates[1];
-
-            PlaceMarker(lat, lon); //create the game object map feature at the coordinates.
+            CreateFeatureVisual(featureObject);
         }
-
-        //right now I'm only parsing the geospatial data but this has to be extended to all of the attribute data contained for each feature.
     }
 
     // Function to create the game object the the heiracrchy procedurally. Takes the lat, lon coordinates as parameters. 
-    private void PlaceMarker(double latitude, double longitude)
+    private void CreateFeatureVisual(JObject feature)
     {
-        GameObject marker = pointPrefab ? Instantiate(pointPrefab) : GameObject.CreatePrimitive(PrimitiveType.Sphere); // For now I'm just creating simple spheres but this will
-                                                                                                                        //change later
-        marker.transform.localScale = Vector3.one * 500.0f; // Scale of spheres needs to be increased dramatically so they are visisble on the map.
+        GameObject marker = pointPrefab ? Instantiate(pointPrefab) : GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        marker.transform.localScale = Vector3.one * 25000f;
+        Collider col = marker.GetComponent<Collider>();
+        col.transform.localScale = Vector3.one * 35000f;
 
-        // Add the marker to the map GameObject immediately to prevent parenting errors later
-        marker.transform.SetParent(map.transform, true); // true to retain world position
+        marker.transform.SetParent(map.transform, true);
 
-        // Delay initialization of the ArcGISLocationComponent (have to wait until the game objects are loaded for each geojson feature)
-        StartCoroutine(InitializeMarkerLocation(marker, latitude, longitude));
-        Debug.Log($"Marker created for latitude: {latitude}, longitude: {longitude}, waiting for initialization."); // Keep this Debug.Log for now
+        FeatureInfo info = marker.AddComponent<FeatureInfo>();
+
+        info.Initialize(feature);
+
+        loadedFeatures.Add(info);
+
+        StartCoroutine(InitializeMarkerLocation(marker, info.Latitude, info.Longitude));
     }
 
     private IEnumerator InitializeMarkerLocation(GameObject marker, double latitude, double longitude)
     {
         while (!map.gameObject.activeInHierarchy)
         {
-            Debug.Log("Waiting for ArcGISMapComponent to become active...");
             yield return null; // Wait for the next frame
         }
 
-        var locationComponent = marker.AddComponent<ArcGISLocationComponent>();
-        locationComponent.Position = new ArcGISPoint(longitude, latitude, defaultAltitude, ArcGISSpatialReference.WGS84());
-
-        Debug.Log($"Marker initialized at latitude: {latitude}, longitude: {longitude}, altitude: {defaultAltitude}");
+        ArcGISLocationComponent locationComponent = marker.AddComponent<ArcGISLocationComponent>();
+        locationComponent.Position = new ArcGISPoint(longitude, latitude, 0, ArcGISSpatialReference.WGS84());     
     }
 }
